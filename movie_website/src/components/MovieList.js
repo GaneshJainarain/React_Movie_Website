7a8a37b1684f1e44cd464cbd41a5424349b7ee38
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-03-18 16:18:17 -0400

    added some dummy data for rendering the movie posters then added a file called movie list in the components folder/ src uses props to pass data then that data is passed to App.js

---
+++ movie_website/src/components/MovieList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MovieList = (props) => {
+    return (
+        <>
+            {props.movies.map((movie, index)=>
+                <div>
+                    <img src={movie.Poster} alt="movie"></img>
+                </div>
+            
+            )}
+        </>
+    )
+}
+
+export default MovieList;
