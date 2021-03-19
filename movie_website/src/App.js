befc33b2e0fd47b95fe04c2d5303f1dc1e7f0ec5
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-03-19 01:18:30 -0400

    replaced the hard coded json with an async await function, the function gets the respinse from the API then gets the data in JSON format, getMovieRequest handles that, then the useEffect hook calls the function, also added some css via classNames, then i passed in the props in the return for the data whihc is the movies, in the MovieList file the props map around the movies, then outputs the poster img using the img tag

--- movie_website/src/App.js
+++ movie_website/src/App.js
@@ -1,38 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import MovieList from "/Users/richeyjay/Desktop/React_Movie_Website/movie_website/src/components/MovieList.js"
 
 const App = () => {
-  const [movies, setMovies] = useState([{
-    "Title": "Star Wars: Episode IV - A New Hope",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VII - The Force Awakens",
-    "Year": "2015",
-    "imdbID": "tt2488496",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-}]);
+  const [movies, setMovies] = useState([]);
+
+const getMovieRequest = async () => {
+
+  const url =  'http://www.omdbapi.com/?s=star wars&apikey=99cdf838&'
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  console.log(responseJson);
+  setMovies(responseJson.Search);
+};
+
+useEffect(() =>{
+  getMovieRequest();
+}, []);
+
   return (
     <div className='container-fluid movie-app'>
       <div className='row'>
